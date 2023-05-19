140e44ab904e369774f1d94d8b8459f1f4d32b65
Lyr <lyr-7d1h@pm.me>
2023-05-19 23:47:13 +0200

    added upload script

--- peripheral/src/main.rs
+++ peripheral/src/main.rs
@@ -1,5 +1,5 @@
 use std::error::Error;
-use std::thread;
+use std::thread::{self};
 use std::time::Duration;
 
 use rppal::gpio::Gpio;
@@ -9,14 +9,13 @@ use rppal::system::DeviceInfo;
 const GPIO_MIC: u8 = 7;
 
 fn main() -> Result<(), Box<dyn Error>> {
-    println!("Blinking an LED on a {}.", DeviceInfo::new()?.model());
-
     let mut pin = Gpio::new()?.get(GPIO_MIC)?.into_output();
 
     // Blink the LED by setting the pin's logic level high for 500 ms.
-    pin.set_high();
-    thread::sleep(Duration::from_millis(500));
-    pin.set_low();
-
-    Ok(())
+    loop {
+        println!("Blinking an LED on a {}.", DeviceInfo::new()?.model());
+        pin.set_high();
+        thread::sleep(Duration::from_millis(500));
+        pin.set_low();
+    }
 }
