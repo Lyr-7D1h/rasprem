4c8115b1f36abb6129f61e4d966085d5361840b4
Lyr <lyr-7d1h@pm.me>
2023-05-20 03:16:16 +0200

    added gpio pins

--- peripheral/src/main.rs
+++ peripheral/src/main.rs
@@ -6,16 +6,23 @@ use rppal::gpio::Gpio;
 use rppal::system::DeviceInfo;
 
 // Gpio uses BCM pin numbering. BCM GPIO 23 is tied to physical pin 16.
-const GPIO_MIC: u8 = 7;
+const GPIO_SCLK: u8 = 11;
+// Master Out Slave In (data output from master)
+const GPIO_MOSI: u8 = 10;
+// Master In Slave Out (data output from slave)
+const GPIO_MISO: u8 = 9;
+// Slave Select: output from master to indicate data being sent
+const GPIO_SS: u8 = 22;
 
 fn main() -> Result<(), Box<dyn Error>> {
-    let mut pin = Gpio::new()?.get(GPIO_MIC)?.into_output();
+    let mut pin = Gpio::new()?.get(23)?.into_output();
 
-    // Blink the LED by setting the pin's logic level high for 500 ms.
     loop {
         println!("Blinking an LED on a {}.", DeviceInfo::new()?.model());
+        // Blink the LED by setting the pin's logic level high for 500 ms.
         pin.set_high();
         thread::sleep(Duration::from_millis(500));
         pin.set_low();
+        thread::sleep(Duration::from_millis(500));
     }
 }
